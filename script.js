1e1a8d7868d10b50e5bb86ad155f5c9a314ef1e0
rewansalah <rewansalahelroubii@gmail.com>
2023-12-10 16:59:50 +0200

    modified

--- script.js
+++ script.js
@@ -2,22 +2,40 @@ function switchLanguage(lang){
     document.documentElement.lang=lang ;
 }
 
+// let valueDisplays = document.querySelectorAll(".num-container");
+// let interval= 1;
+
+
+// valueDisplays.forEach(valueDisplay =>{
+//     let startValue = 0 ;
+//     let endValue = parseInt(valueDisplay.getAttribute
+//         ("data-val"));
+
+//    let duration = Math.floor(interval / endValue);
+//    let counter = setInterval(function(){
+
+//     startValue += 1 ;
+//     valueDisplay.textContent = startValue ;
+
+//    });
+
+
+// });
+
 let valueDisplays = document.querySelectorAll(".num-container");
-let interval= 1;
+let interval = 1;
 
+valueDisplays.forEach(valueDisplay => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
 
-valueDisplays.forEach(valueDisplay =>{
-    let startValue = 0 ;
-    let endValue = parseInt(valueDisplay.getAttribute
-        ("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function () {
+        startValue += 4;
+        valueDisplay.textContent = startValue;
 
-   let duration = Math.floor(interval / endValue);
-   let counter = setInterval(function(){
-
-    startValue += 1 ;
-    valueDisplay.textContent = startValue ;
-
-   });
-
-
+        if (startValue >= endValue) {
+            clearInterval(counter); // Stop the counter when it reaches the specified value
+        }
+    }, duration);
 });
